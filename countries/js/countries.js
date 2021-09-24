b7e8c531e6d115844e99c9e523ca282d6630c729
1Andry <rivsmile@yandex.ru>
2021-09-24 22:23:37 +0300

    countries

--- countries/js/countries.js
+++ countries/js/countries.js
@@ -4,7 +4,6 @@
  * @param {string[]} countries
  */
 export const getDropdown = (countries) => {
-    
     return `<option value="netherlands">${countries[0]}</option>
             <option value="japan">${countries[1]}</option>
             <option value="mongolia">${countries[2]}</option>`;
